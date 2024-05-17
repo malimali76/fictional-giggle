0aa94e4be7c32048a8389e824e65b381f19dae81
malimali76 <78163341+malimali76@users.noreply.github.com>
2024-05-17 02:43:07 -0400

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,16 +1,47 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Pressable, FlatList } from 'react-native';
+
+//Figure out the error when navigating from create shipment screen. Parameter Error
+
+const uri = 'http://192.168.0.7:3000/'
 
 export function HomeScreen({navigation, route}) {
-    const {parameters} = route.params
+  
+      const {parameters} = route.params;
+      shipperID = parameters.shipperID;
 
-    
+    const [shipments, setShipments] = useState([])
 
-    return (
-      <View style={styles.container}>
-        <Text>{parameters.email}</Text>
-      </View>
+    const response = fetch(uri + 'dashboard/' + parameters.shipperID)
+    .then(response => response.json())
+    .then(data => {
+      setShipments(data);
+    })
+    .catch(error => console.error(error))
+
+    return(
+      <View  style={styles.container}>
+          <Text>Shipments:</Text>
+          <FlatList
+            data={shipments}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <View style={styles.container}>
+                <Pressable style={styles.shipment} onPress={() => {navigation.navigate('ShipmentDetails', {shipment: item})}}>
+                <Text style={styles.text}>{item.location} to {item.destination}</Text>
+                </Pressable>
+                
+                {/* Render other shipment properties as needed */}
+              </View>
+            )}
+          />
+
+          <View>
+          <Pressable style={styles.shipment} onPress={() => {navigation.navigate('NewShipment', shipperID)}}>
+          <Text  style={styles.text}>Ceate A new Shipment</Text>
+          </Pressable>
+          </View>
+        </View>
     )
 }
 
@@ -45,10 +76,22 @@ const styles = StyleSheet.create({
       margin: 10
     },
     text: {
-      fontSize: 16,
+      fontSize: 14,
       lineHeight: 21,
       fontWeight: 'bold',
       letterSpacing: 0.25,
       color: 'white',
+    },
+    shipment: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 5,
+      paddingHorizontal: 16,
+      borderRadius: 10,
+      width: 300,
+      height: 50,
+      elevation: 3,
+      backgroundColor: 'black',
+      margin: 15
     },
   });
