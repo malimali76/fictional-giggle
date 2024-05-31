06ca48a01540d4d6bafd626cee7295147006a279
malimali76 <78163341+malimali76@users.noreply.github.com>
2024-05-31 08:25:13 -0400

    Update Styles.js

--- Styles.js
+++ Styles.js
@@ -1,10 +1,11 @@
 import { StyleSheet} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: 'black',
+      backgroundColor: Colors.white,
       alignItems: 'center',
       justifyContent: 'flex-end'
     },
@@ -16,17 +17,17 @@ export const styles = StyleSheet.create({
     borderRadius: 15,
     elevation: 3,
     margin: 10,
-    backgroundColor: 'white'
+    backgroundColor: Colors.light,
+    color: Colors.black,
+    fontWeight: 'bold'
     },
     Pillbutton: {
-      backgroundColor: 'lightgreen',
+      backgroundColor: Colors.primary,
       height: 40,
       width: 100,
       justifyContent: 'center',
       alignItems: 'center',
       borderRadius: 50,
-      borderWidth: 1,
-      borderColor: 'green',
       margin: 10
     },
     text: {
@@ -34,6 +35,55 @@ export const styles = StyleSheet.create({
       lineHeight: 21,
       fontWeight: 'bold',
       letterSpacing: 0.25,
-      color: 'black',
+      color: Colors.white,
+    },
+    darktext: {
+      fontSize: 16,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: Colors.black,
+    },
+    header2:{
+      fontSize: 18,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: Colors.primary,
+    },
+    regtext: {
+      fontSize: 15,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: 'white',
+    },
+    headerText: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: 'white',
+      margin: 30
+    },
+    textInputContainer: {
+      backgroundColor: 'rgba(0,0,0,0)',
+      borderTopWidth: 0,
+      borderBottomWidth: 0,
+      width: 300,
+    },
+    textInput: {
+      marginLeft: 0,
+      marginRight: 0,
+      height: 38,
+      width: 200,
+      color: '#5d5d5d',
+      fontSize: 16,
+    },
+    listView: {
+      backgroundColor: 'brown',
+
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
     },
   });
