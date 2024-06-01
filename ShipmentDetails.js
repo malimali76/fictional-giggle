6f21aa7775ed2d8de5089907c10248c9cc363ab3
malimali76 <78163341+malimali76@users.noreply.github.com>
2024-06-01 16:44:27 -0700

    Update ShipmentDetails.js

--- ShipmentDetails.js
+++ ShipmentDetails.js
@@ -5,21 +5,40 @@ import { styles } from './assets/Styles';
 
 export function ShipmentDetails({navigation, route}){
     const parameters = route.params.shipment;
+    const driverID = route.params.driverid;
+    console.log(route.params)
 
     function deleteshipment(){
-null
+      null
+          }
+
+    if(driverID){
+      return(
+        <View style={styles.container}>
+          <Text style={styles.darktext}>Title: {parameters.title}</Text>
+          <Text style={styles.darktext}>Pick Up Location: {parameters.location.description}</Text>
+          <Text style={styles.darktext}>Pick Up Destination: {parameters.destination.description}</Text>
+          <Text style={styles.darktext}>Category: {parameters.category}</Text>
+          <Text style={styles.darktext}>Description: {parameters.description}</Text>
+          <Text style={styles.darktext}>Item Amount: {parameters.itemAmount}</Text>
+          <Pressable style={[styles.Pillbutton, {width: 150}]} onPress={deleteshipment}><Text style={styles.text}>Place Bid</Text></Pressable>
+        </View>
+      )
+    }
+    else{
+      return(
+        <View style={styles.container}>
+          <Text style={styles.darktext}>Title: {parameters.title}</Text>
+          <Text style={styles.darktext}>Pick Up Location: {parameters.location.description}</Text>
+          <Text style={styles.darktext}>Pick Up Destination: {parameters.destination.description}</Text>
+          <Text style={styles.darktext}>Category: {parameters.category}</Text>
+          <Text style={styles.darktext}>Description: {parameters.description}</Text>
+          <Text style={styles.darktext}>Item Amount: {parameters.itemAmount}</Text>
+          <Pressable style={[styles.Pillbutton, {width: 150}]} onPress={deleteshipment}><Text style={styles.text}>delete shipment</Text></Pressable>
+          <Pressable style={[styles.Pillbutton, {width: 150}]} onPress={deleteshipment}><Text style={styles.text}>Update shipment</Text></Pressable>
+  
+        </View>
+      )
     }
 
-    return(
-      <View style={styles.container}>
-        <Text style={styles.darktext}>Title: {parameters.title}</Text>
-        <Text style={styles.darktext}>Pick Up Location: {parameters.location.description}</Text>
-        <Text style={styles.darktext}>Pick Up Destination: {parameters.destination.description}</Text>
-        <Text style={styles.darktext}>Category: {parameters.category}</Text>
-        <Text style={styles.darktext}>Description: {parameters.description}</Text>
-        <Text style={styles.darktext}>Item Amount: {parameters.itemAmount}</Text>
-        <Pressable style={[styles.Pillbutton, {width: 150}]} onPress={deleteshipment}><Text style={styles.text}>Place Bid</Text></Pressable>
-
-      </View>
-    )
 }
