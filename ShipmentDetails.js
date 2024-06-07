327ddc8a1d7889219ccef1d799b02810c3b9257e
malimali76 <78163341+malimali76@users.noreply.github.com>
2024-06-07 00:44:20 -0400

    Update ShipmentDetails.js

--- ShipmentDetails.js
+++ ShipmentDetails.js
@@ -1,44 +1,70 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, Pressable, FlatList, SafeAreaView } from 'react-native';
-import {uri} from './assets/uri'
+import { uri } from './assets/uri'
 import { styles } from './assets/Styles';
 
-export function ShipmentDetails({navigation, route}){
-    const parameters = route.params.shipment;
-    const driverID = route.params.driverid;
-    console.log(route.params)
+export function ShipmentDetails({ navigation, route }) {
+  const shipment = route.params.shipment;
+  const driverid = route.params.driverid;
+  const shipmentid = route.params.shipment._id
+  console.log(route.params)
 
-    function deleteshipment(){
-      null
-          }
+  function deleteshipment() {
+    null
+  }
 
-    if(driverID){
-      return(
-        <View style={styles.container}>
-          <Text style={styles.darktext}>Title: {parameters.title}</Text>
-          <Text style={styles.darktext}>Pick Up Location: {parameters.location.description}</Text>
-          <Text style={styles.darktext}>Pick Up Destination: {parameters.destination.description}</Text>
-          <Text style={styles.darktext}>Category: {parameters.category}</Text>
-          <Text style={styles.darktext}>Description: {parameters.description}</Text>
-          <Text style={styles.darktext}>Item Amount: {parameters.itemAmount}</Text>
-          <Pressable style={[styles.Pillbutton, {width: 150}]} onPress={deleteshipment}><Text style={styles.text}>Place Bid</Text></Pressable>
-        </View>
-      )
+  async function placebid() {
+    bid_data = {driverid, shipmentid}
+    if(driverid && shipmentid){
+      try {
+        const response = await fetch(uri + 'new-bid/' + driverid, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bid_data),
+        }
+      );
+      } 
+      catch (error) {
+        console.error("Error:", error);
+      }
     }
     else{
-      return(
-        <View style={styles.container}>
-          <Text style={styles.darktext}>Title: {parameters.title}</Text>
-          <Text style={styles.darktext}>Pick Up Location: {parameters.location.description}</Text>
-          <Text style={styles.darktext}>Pick Up Destination: {parameters.destination.description}</Text>
-          <Text style={styles.darktext}>Category: {parameters.category}</Text>
-          <Text style={styles.darktext}>Description: {parameters.description}</Text>
-          <Text style={styles.darktext}>Item Amount: {parameters.itemAmount}</Text>
-          <Pressable style={[styles.Pillbutton, {width: 150}]} onPress={deleteshipment}><Text style={styles.text}>delete shipment</Text></Pressable>
-          <Pressable style={[styles.Pillbutton, {width: 150}]} onPress={deleteshipment}><Text style={styles.text}>Update shipment</Text></Pressable>
-  
-        </View>
-      )
+      console.error('fuckry graan')
     }
+    } 
+
+
+  if (driverid) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.darktext}>Title: {shipment.title}</Text>
+        <Text style={styles.darktext}>Pick Up Location: {shipment.location.description}</Text>
+        <Text style={styles.darktext}>Pick Up Destination: {shipment.destination.description}</Text>
+        <Text style={styles.darktext}>Category: {shipment.category}</Text>
+        <Text style={styles.darktext}>Description: {shipment.description}</Text>
+        <Text style={styles.darktext}>Item Amount: {shipment.itemAmount}</Text>
+        <Pressable style={[styles.Pillbutton, { width: 150 }]} onPress={() => {placebid(), navigation.navigate('DriverDashboard', {driverid} )}}><Text style={styles.text}>Place Bid</Text></Pressable>
+      </View>
+    )
+
+  }
+  else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.darktext}>Title: {shipment.title}</Text>
+        <Text style={styles.header3}>Pick Up Location: {shipment.location.description}</Text>
+        <Text style={styles.header3}>Pick Up Destination: {shipment.destination.description}</Text>
+        <Text style={styles.darktext}>Category: {shipment.category}</Text>
+        <Text style={styles.darktext}>Description: {shipment.description}</Text>
+        <Text style={styles.darktext}>Item Amount: {shipment.itemAmount}</Text>
+        <Text style={styles.darktext}>driver: {shipment.driverid}</Text>
+        <Pressable style={[styles.Pillbutton, { width: 150 }]} onPress={deleteshipment}><Text style={styles.text}>delete shipment</Text></Pressable>
+        <Pressable style={[styles.Pillbutton, { width: 150 }]} onPress={deleteshipment}><Text style={styles.text}>Update shipment</Text></Pressable>
+      </View>
+    )
+
+  }
 
 }
