0f093f18c52011aecfb25488b6861cb23dc659c6
malimali76 <78163341+malimali76@users.noreply.github.com>
2024-06-07 00:42:06 -0400

    Update NewShipments.js

--- NewShipments.js
+++ NewShipments.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Pressable, FlatList, SafeAreaView, TextInput } from 'react-native';
-import { HomeScreen } from './Dashboard';
+import React, { useState } from 'react'
+import { Text, View, Pressable, TextInput, StyleSheet } from 'react-native';
 import { styles } from './assets/Styles';
-import {uri} from './assets/uri'
+import { uri } from './assets/uri'
+import { SelectList } from 'react-native-dropdown-select-list'
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export function NewShipment({navigation, route}){
-    shipperid = route.params;
-    shipperID = route.params;
+export function NewShipment({ navigation, route }) {
+  shipperid = route.params;
 
-    const [title, setTitle] = useState('')
-    const [location, setLocation] = useState('')
-    const [destination, setDestination] = useState('')
-    const [category, setCategory] = useState('')
-    const [description, setDescription] = useState('')
-    const [itemAmount, setItemAmount] = useState('')
+  const [category, setCategory] = useState(null)
+  const [title, setTitle] = useState(null)
+  const [description, setDescription] = useState(null)
+  const [itemAmount, setItemAmount] = useState(null)
+
+  const data = [
+    { key: '1', value: 'Moves' },
+    { key: '2', value: 'Household Item' },
+    { key: '3', value: 'Vehicle' },
+    { key: '4', value: 'Heavy Equipment' },
+  ]
+
+function setSelected(val){
+  setCategory(val);
+}
 
   const onChangeTextTitle = (inputText) => {
     setTitle(inputText);
-  };
-  const onChangeTextLocation = (inputText) => {
-    setLocation(inputText);
-  };
-  const onChangeTextDestination = (inputText) => {
-    setDestination(inputText);
-  };
-  const onChangeTextCategory = (inputText) => {
-    setCategory(inputText);
   };
   const onChangeTextDescription = (inputText) => {
     setDescription(inputText);
@@ -34,46 +35,64 @@ export function NewShipment({navigation, route}){
     setItemAmount(inputText);
   };
 
-  const shipmentData = { title, location, destination, category, description, itemAmount, shipperid }
+  const shipmentData = { title, category, description, itemAmount, shipperid }
 
   async function NewShipment(shipmentData) {
-      try {
-        const response = await fetch(uri + 'new-shipment/', {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(shipmentData),
-        });
-      } 
-      catch (error) {
-        console.error("Error:", error);
-      }
+    try {
+      const response = await fetch(uri + 'new-shipment/', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(shipmentData),
+      });
+    }
+    catch (error) {
+      console.error("Error:", error);
+    }
 
   }
 
+  async function nextPage(){
+    if(category){
+      navigation.navigate('SetLocation', {shipperid, title, category, description, itemAmount})
+    }
+    else{
+      console.error('Choose a shipment category')
+    }
+  }
+
   function CreateShipment() {
-    if(location != ''){
+    if (location != '') {
       console.log('Shipment Created')
       NewShipment(shipmentData)
       navigation.goBack();
     }
-    else{
+    else {
       console.error('Enter A PickUp Location')
     }
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Create A New Shipment</Text>
-      <TextInput style={styles.TextBox} placeholderTextColor="lightgrey" onChangeText={onChangeTextTitle} value={title} placeholder='Shipment Title'></TextInput>
-      <TextInput style={styles.TextBox} placeholderTextColor="lightgrey" onChangeText={onChangeTextLocation} value={location} placeholder='Pick Up Location'></TextInput>
-      <TextInput style={styles.TextBox} placeholderTextColor="lightgrey" onChangeText={onChangeTextDestination} value={destination} placeholder='Destination'></TextInput>
-      <TextInput style={styles.TextBox} placeholderTextColor="lightgrey" onChangeText={onChangeTextCategory} value={category} placeholder='Shipment Category'></TextInput>
-      <TextInput style={styles.TextBox} placeholderTextColor="lightgrey" onChangeText={onChangeTextDescription} value={description} placeholder='Description'></TextInput>
-      <TextInput style={styles.TextBox} placeholderTextColor="lightgrey" onChangeText={onChangeTextItemAmount} value={itemAmount} placeholder='Item Amount'></TextInput>
+    <View style={[{ paddingTop: 50, padding: 10 }]}>
+      <Text style={[styles.header2, { marginBottom: 20 }]}>Category</Text>
+      <SelectList
+        setSelected={(val) => setSelected(val)}
+        data={data}
+        save="value"
+      />
 
-      <Pressable style={[styles.Pillbutton, {width: 160}]} onPress={CreateShipment}><Text style={styles.text}>Create Shipmentt</Text></Pressable>
+      <TextInput style={styles.TextBox} onChangeText={onChangeTextTitle} value={title} placeholder='Title'></TextInput>
+      <TextInput style={styles.TextBox} onChangeText={onChangeTextDescription} value={description} placeholder='Description'></TextInput>
+      <TextInput style={styles.TextBox} onChangeText={onChangeTextItemAmount} value={itemAmount} placeholder='Item Amount'></TextInput>
+
+      <Pressable onPress={()=>{nextPage()}} style={{
+        height: 60, width: '100%', backgroundColor: Colors.lighter, padding: 10,
+        alignItems: 'center', borderBottomWidth: StyleSheet.hairlineWidth, flexDirection: 'row', marginTop: 10
+      }}>
+        <Text style={styles.header3}>Continue</Text>
+        <Icon name='chevron-right' size={15} style={{ position: 'absolute', right: 10 }} />
+      </Pressable>
 
     </View>
   )
