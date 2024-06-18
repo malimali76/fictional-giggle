991d0eff3e8cef7e9370c1c7946f3841fcc987ce
malimali76 <78163341+malimali76@users.noreply.github.com>
2024-06-18 09:00:43 -0400

    Update setlocation.js

--- setlocation.js
+++ setlocation.js
@@ -13,11 +13,11 @@ export function Setlocation({ navigation, route }) {
     parameters = route.params;
     shipperid = route.params.shipperid;
 
-    function gotodestination (){
-        if(location){
-            navigation.navigate('SetDestination', {location, shipperid, parameters})
+    function gotodestination() {
+        if (location) {
+            navigation.navigate('SetDestination', { location, shipperid, parameters })
         }
-        else{
+        else {
             console.error('Enter A pick up location')
         }
     }
@@ -25,49 +25,62 @@ export function Setlocation({ navigation, route }) {
     const [location, setLocation] = useState(null)
 
     return (
-        <View style={[styles.container, {paddingTop: 50}]}>
-            
-            <Pressable onPress={gotodestination} style={{height: 60, width: '100%', backgroundColor:Colors.white, padding: 10,
-             alignItems:'center', borderBottomWidth: StyleSheet.hairlineWidth, flexDirection:'row', marginBottom: 20}}>
-                <Text style={styles.header2}>Continue</Text>
-                <Icon  name='chevron-right' size={20} style={{ position: 'absolute', right: 10 }}/>
-            </Pressable>
+        <View style={styles.container2}>
 
-            <GooglePlacesAutocomplete
-                placeholder="Enter pick up location"
-                minLength={2}
-                fetchDetails={true}
-                onPress={(data, details = null) => {
-                    setLocation({
-                        description: data.description,
-                        latitude: details.geometry.location.lat,
-                        longitude: details.geometry.location.lng,
-                    })
-                }}
-                query={{
-                    key: GOOGLE_PLACES_API_KEY,
-                    language: 'en',
-                }}
-                styles={{
-                    textInputContainer: {
-                        backgroundColor: 'rgba(0,0,0,0)',
-                        borderTopWidth: 0,
-                        borderBottomWidth: 0,
-                        width: '100%',
-                    },
-                    textInput: {
-                        marginLeft: 0,
-                        marginRight: 0,
-                        height: 38,
-                        color: Colors.dark,
-                        fontSize: 16,
-                        backgroundColor: Colors.light
-                    },
-                    description: {
-                    },
-                }}
-            />
 
+            <View style={styles.headerTab}>
+                <Text style={styles.header3}>SET LOCAION</Text>
+            </View>
+
+            <View style={styles.background}>
+                <View style={{height: 320}}>
+                <GooglePlacesAutocomplete
+                    placeholder="Enter pick up location"
+                    minLength={2}
+                    fetchDetails={true}
+                    onPress={(data, details = null) => {
+                        setLocation({
+                            description: data.description,
+                            latitude: details.geometry.location.lat,
+                            longitude: details.geometry.location.lng,
+                        })
+                    }}
+                    query={{
+                        key: GOOGLE_PLACES_API_KEY,
+                        language: 'en',
+                    }}
+                    styles={{
+                        textInputContainer: {
+                            backgroundColor: 'rgba(0,0,0,0)',
+                            borderTopWidth: 0,
+                            borderBottomWidth: 0,
+                            width: '100%',
+                        },
+                        textInput: {
+                            marginLeft: 0,
+                            marginRight: 0,
+                            height: 38,
+                            color: Colors.dark,
+                            fontSize: 16,
+                            backgroundColor: Colors.light
+                        },
+                        description: {
+                        },
+                    }}
+                />
+                </View>
+                
+
+                <Pressable style={{
+                    height: 60, width: '100%', backgroundColor: Colors.white, padding: 10,
+                    alignItems: 'center', flexDirection: 'row', marginBottom: 20,
+                    marginTop: 30, elevation: 3, borderRadius: 10
+                }} onPress={gotodestination}>
+                    <Text style={styles.header3}>Next</Text>
+                    <Icon name='chevron-right' size={15} style={{ position: 'absolute', right: 10 }} />
+                </Pressable>
+
+            </View>
 
         </View>
     )
