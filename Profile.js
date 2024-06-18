25265d3e7921562d9fb791b4a4f45c8039b2e8b7
malimali76 <78163341+malimali76@users.noreply.github.com>
2024-06-18 09:00:00 -0400

    Update Profile.js

--- Profile.js
+++ Profile.js
@@ -1,68 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Pressable } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
+import { styles } from './assets/Styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const uri = 'http://192.168.0.11:3000/'
 
-export function Profile({navigation, route}) {
-    const {parameters} = route.params
-
-    const [firstname, setFirstName] = useState('')
-    const [lastname, setLastName] = useState('')
-    const [phone, setPhone] = useState('')
-
-    fetch(uri + 'login/' + parameters.email)
-        .then(response => response.json())
-        .then(data => {
-          setFirstName(data.firstname),
-          setLastName(data.lastname),
-          setPhone(data.phone);
-        })
-        .catch(error => console.error(error))
-
-    return (
-      <View style={styles.container}>
-        <Text>Dashboard</Text>
-      </View>
+export function Profile({route}){
+    return(
+        <View styles={styles.container2}>
+            <Text>Profile</Text>
+        </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-  
-    },
-    TextBox: {
-      borderWidth: 2,
-      height: 50,
-      margin: 10,
-      width: 300,
-      backgroundColor: 'black',
-      borderRadius: 5,
-      paddingVertical: 6,
-      paddingHorizontal: 16,
-      color: 'white',
-    },
-    button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 6,
-      paddingHorizontal: 16,
-      borderRadius: 5,
-      width: 200,
-      elevation: 3,
-      backgroundColor: 'black',
-      margin: 10
-    },
-    text: {
-      fontSize: 16,
-      lineHeight: 21,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'white',
-    },
-  });
